afbd5c8915db6130456fe121149bff625b85156a
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2019-09-29 16:24:56 -0700

    added routes to handle 404s and 500 errors

--- src/index.js
+++ src/index.js
@@ -4,8 +4,25 @@ let app = express()
 
 let personRoute = require('./routes/person')
 
+let path = require('path')
+app.use((req, res, next) => {
+	console.log(`${new Date().toString()} => ${req.originalUrl}`)
+	next()
+})
+
 app.use(personRoute)
 app.use(express.static('public'))
+
+// Handler for 404 
+app.use((req, res, next) => {
+	res.status(404).send('We think you are lost!')
+})
+
+// Handler for error 500
+app.use((err, req, res, next) => {
+	console.error(err.stack)
+	res.sendFile(path.join(__dirname, '../public/500.html'))
+})
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.info(`Server has started on ${PORT}`))
