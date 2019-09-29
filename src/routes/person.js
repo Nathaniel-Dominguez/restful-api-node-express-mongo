b0f2bc450619f2d6d073283c5c1a6b111a22cfdb
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2019-09-29 15:43:50 -0700

    tested the person and person/name on postman

--- src/routes/person.js
+++ src/routes/person.js
@@ -5,4 +5,9 @@ router.get('/person', (req, res) => {
 	res.send('You have requsted a person')
 })
 
+router.get('/person/:name', (req, res) => {
+
+	res.send(`You have requsted a person ${req.params.name}`)
+})
+
 module.exports = router
